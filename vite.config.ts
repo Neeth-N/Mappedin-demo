55bb9920b9f38e562dc1f486757c518f1920556b
Neeth <just.neeth@gmail.com>
2025-06-22 20:12:02 +0530

    Github Pages v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: 'Mappedin-demo', // important!
+  plugins: [react()],
+  base: '/Mappedin-demo/', // 🔥 Required for GitHub Pages
 })
